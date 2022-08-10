bac5a1bc4a6aed3eb1c0710ffd600c3e7127fefd
Guilherme Moura <guiavilamou@gmail.com>
2022-08-10 16:24:45 -0300

    novas atualizacoes comentarios

--- Trabalho.java
+++ Trabalho.java
@@ -2,20 +2,16 @@ import java.io.*;
 
 public class Trabalho {
     public static void main(String[] args) {
-
-        //CSV = Comma-Separated Values 
-        //   text file that uses a comma to separate values
-
-        // The path of the file that contains the data.
+        // O caminho do arquivo que contem os dados.
         String file = "Entrevistados.csv";
-        // Creating a variable called reader of type BufferedReader and initializing it with null.
+        // Criacao de uma variavel chamada reader do tipo BufferedReader e inicializando como null.
         BufferedReader reader = null;
-        // Creating a variable called line of type String and initializing it with an empty string.
+        // Criacao de uma variavel chamada line do tipo String e inicializando como uma String vazia.
         String line = " ";
-        // Creating an object of the class Evetor.
+        // Criacao de um objeto da classe Evetor.
         String conc = " ";
         Evetor v = new Evetor ();
-       // Reading the file and creating an object of the class Entrevistado for each line of the file.
+       // Leitura do arquivo e criação de um objeto da classe Entrevistado para cada linha do arquivo.
         try {
             reader = new BufferedReader(new FileReader(file));
             while((line = reader.readLine()) != null) {
@@ -36,28 +32,30 @@ public class Trabalho {
                 e.printStackTrace();
             }
         }
-       // Printing the result of the method relatorio1() of the class Evetor.
-       System.out.println(v.relatorioQuantPessoas()); 
-       // Printing the result of the method relatorio2() of the class Evetor.
-       System.out.println(v.relatorioSexo()); 
-       // Printing the result of the method relatorio3() of the class Evetor.
-       System.out.println(v.relatorioFaixaEtaria()); 
-       // Printing the result of the method relatorio4() of the class Evetor.
-       System.out.println(v.relatorioEscolaridade()); 
-       // Printing the result of the method relatorio5() of the class Evetor.
-       System.out.println(v.relatorioEnsiSuperior());
-       // Printing the result of the method relatorio6() of the class Evetor.
-       System.out.println(v.relatorioMaisUtilizaCelular());  
-       // Printing the result of the method relatorio7() of the class Evetor.
-       System.out.println(v.relatorioTecMenosUtil15());  
-       // Printing the result of the method relatorio8() of the class Evetor.
-       System.out.println(v.relatorioAreaPrioritaria()); 
-       conc = "Informações pedidas pelo trabalho que foram encontradas pelo grupo: \n" + v.relatorioQuantPessoas() + "\n" + v.relatorioSexo() + "\n" + v.relatorioFaixaEtaria() + "\n" + v.relatorioEscolaridade() + "\n" + v.relatorioEnsiSuperior() + "\n" + v.relatorioMaisUtilizaCelular() + "\n" + v.relatorioTecMenosUtil15() + "\n" + v.relatorioAreaPrioritaria();
-       try {
-        Arquivo.gravar(conc,"Relatorio.txt");
-    } catch (Exception e) {
-    
-        e.printStackTrace();
-    }
+        // Escreve o resultado do método relatorioQuantPessoas() da classe Evetor.
+        System.out.println(v.relatorioQuantPessoas()); 
+        // Escreve o resultado do método relatorioSexo() da classe Evetor.
+        System.out.println(v.relatorioSexo()); 
+        // Escreve o resultado do método relatorioFaixaEtaria() da classe Evetor.
+        System.out.println(v.relatorioFaixaEtaria()); 
+        // Escreve o resultado do método relatorioEscolaridade() da classe Evetor.
+        System.out.println(v.relatorioEscolaridade()); 
+        // Escreve o resultado do método relatorioEnsiSuperior() da classe Evetor.
+        System.out.println(v.relatorioEnsiSuperior());
+        // Escreve o resultado do método relatorioMaisUtilizaCelular() da classe Evetor.
+        System.out.println(v.relatorioMaisUtilizaCelular());  
+        // Escreve o resultado do método relatorioTecMenosUtil15() da classe Evetor.
+        System.out.println(v.relatorioTecMenosUtil15());  
+        // Escreve o resultado do método relatorioAreaPrioritaria() da classe Evetor.
+        System.out.println(v.relatorioAreaPrioritaria()); 
+        // Concatena os resultados dos metodos da classe Evetor.
+        conc = "Informações pedidas pelo trabalho que foram encontradas pelo grupo: \n" + v.relatorioQuantPessoas() + "\n" + v.relatorioSexo() + "\n" + v.relatorioFaixaEtaria() + "\n" + v.relatorioEscolaridade() + "\n" + v.relatorioEnsiSuperior() + "\n" + v.relatorioMaisUtilizaCelular() + "\n" + v.relatorioTecMenosUtil15() + "\n" + v.relatorioAreaPrioritaria();
+        try {
+            // Escreve o resultado da concatenacao dos metodos da classe Evetor em um arquivo
+            // chamado Relatorio.txt.
+            Arquivo.gravar(conc,"Relatorio.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
